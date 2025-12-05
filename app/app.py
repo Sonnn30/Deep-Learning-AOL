e3a3a17cbc9a43657c85a9ba253d0a26fdae97b7
Wilson P <wilsonprajnawira345@gmail.com>
2025-12-05 15:36:21 +0700

    update app

---
+++ app/app.py
@@ -0,0 +1,37 @@
+import gradio as gr
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import json
+
+
+model = tf.keras.models.load_model(
+    "model_Final.keras",
+    custom_objects={
+        "preprocess_input": tf.keras.applications.resnet50.preprocess_input
+    }
+)
+with open('labels.json', 'r') as f:
+    class_label = json.load(f)
+
+
+def predict_input(img):
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)[0]
+    idx = np.argmax(pred)
+
+    return class_label[idx]
+
+
+demo = gr.Interface(
+    fn=predict_input,
+    inputs=gr.Image(type="pil"),
+    outputs=gr.Label()
+
+)
+
+
+demo.launch()
